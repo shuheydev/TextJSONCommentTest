62742ee5dc1b3abd50276cef6da17adc803ff5e5
shuheydev <shuhey.dev@gmail.com>
2020-07-10 17:44:52 +0900

    Add project files.

---
+++ TextJSONCommentTest/Models/Sample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextJSONCommentTest.Models
+{
+
+    public class SampleData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+}
---
+++ TextJSONCommentTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text.Json;
+using TextJSONCommentTest.Models;
+
+namespace TextJSONCommentTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using var stream = assembly.GetManifestResourceStream("TextJSONCommentTest.Data.sampleData.jsonc");
+
+            //read test
+            using var sr = new StreamReader(stream);
+            string jsonString = sr.ReadToEnd();
+            Console.WriteLine(jsonString);
+
+
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                ReadCommentHandling = JsonCommentHandling.Skip
+            };
+
+            //deserialize 
+            SampleData sampleData = System.Text.Json.JsonSerializer.Deserialize<SampleData>(jsonString, options);
+
+            //deserialize test
+            Console.WriteLine(sampleData.FirstName);
+        }
+    }
+}
